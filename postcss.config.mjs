c44ba3d392817ca3031193de726a3e1e32159fc4
Vitaliy Vanyushin <wm@codehtml.ru>
2026-04-07 18:14:28 +0400

    set fonts

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,6 +1,20 @@
+/** @type {import('postcss-load-config').Config} */
 const config = {
   plugins: {
-    "@tailwindcss/postcss": {},
+    '@tailwindcss/postcss': {},
+    'postcss-flexbugs-fixes': {},
+    'postcss-nested': {},
+    'postcss-preset-env': [
+      {
+        autoprefixer: {
+          flexbox: 'no-2009',
+        },
+        stage: 3,
+        features: {
+          'custom-properties': false,
+        },
+      },
+    ],
   },
 };
 
